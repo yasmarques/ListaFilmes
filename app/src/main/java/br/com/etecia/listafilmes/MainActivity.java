1682cf4e50f64512d8659ec5bc16f5f5b8947cee
yasmarques <yasmin.marquestito14@gmail.com>
2022-12-01 19:58:53 -0300

    feature

--- app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
+++ app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
@@ -3,12 +3,50 @@ package br.com.etecia.listafilmes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView ListaFilmes;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListaFilmes = findViewById(R.id.idListaFilmes);
+
+        MyAdapter adapter = new MyAdapter();
+
+        ListaFilmes.setAdapter(adapter);
+    }
+
+    //criando inner class MyAdapter
+
+    public class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
